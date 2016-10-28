6c4ae63a5211fb72f4248ee97e284bd06c34c938
krshivam <kumarshivam675@gmail.com>
2016-10-28 19:44:23 +0530

    batsmen bowler data generator directory added. the genrated data also available now in the main directory

---
+++ Batsmen-Bowler-Data-Generator/batsmen_bowler.py
@@ -0,0 +1,67 @@
+from os import listdir
+
+import yaml
+
+import savetocsv
+
+
+def get_filenames():
+    return listdir('./data')
+
+
+def structure_data_dictionary(data):
+    for key in data:
+        batsman = key
+        for key2 in data[key]:
+            bowler = key2
+            zero = data[key][key2][0]
+            one = data[key][key2][1]
+            two = data[key][key2][2]
+            three = data[key][key2][3]
+            four = data[key][key2][4]
+            five = data[key][key2][5]
+            six = data[key][key2][6]
+            wicket = data[key][key2][7]
+
+            savetocsv.save(batsman, bowler, zero, one, two, three, four, five, six, wicket)
+
+
+def extract_info():
+    data = {}
+    count = 0
+    innings_list = ["1st innings", "2nd innings"]
+    for file in get_filenames():
+        print file, count
+        count += 1
+        try:
+            f = open('./data/' + file)
+            match = yaml.safe_load(f)
+            for innings in innings_list:
+                for ball in match['innings'][innings_list.index(innings)][innings]["deliveries"]:
+                    for key in ball:
+                        runs = ball[key]["runs"]["batsman"]
+                        if ball[key]["batsman"] in data:
+                            if ball[key]["bowler"] not in data[ball[key]["batsman"]]:
+                                data[ball[key]["batsman"]][ball[key]["bowler"]] = [0, 0, 0, 0, 0, 0, 0, 0]
+
+                            if "wicket" in ball[key]:
+                                    data[ball[key]["batsman"]][ball[key]["bowler"]][7] += 1
+                            else:
+                                data[ball[key]["batsman"]][ball[key]["bowler"]][runs] += 1
+
+                        else:
+                            data[ball[key]["batsman"]] = {}
+                            data[ball[key]["batsman"]][ball[key]["bowler"]] = [0, 0, 0, 0, 0, 0, 0, 0]
+
+                            if "wicket" in ball[key]:
+                                    data[ball[key]["batsman"]][ball[key]["bowler"]][7] += 1
+                            else:
+                                data[ball[key]["batsman"]][ball[key]["bowler"]][runs] += 1
+            f.close()
+
+        except:
+            print "error in file"
+
+    structure_data_dictionary(data)
+
+extract_info()
