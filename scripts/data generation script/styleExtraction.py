8fb1d93e66892fea51e20e96921d302ba2861d90
aditya9509 <Aditya.Naidu@iiitb.org>
2016-11-01 12:33:58 +0530

    Player bowling style extraction script

---
+++ scripts/data generation script/styleExtraction.py
@@ -0,0 +1,29 @@
+#Extracts batting and bowling style of players
+#l = []
+mapping = {'Right-arm medium' :('Right-arm','medium'), 'Right-arm offbreak':('Right-arm','offbreak'), 'Right-arm fast-medium':('Right-arm','fast-medium'), 'Legbreak googly':(None,'legbreak'), 'Right-arm medium-fast':('Right-arm','medium-fast'), 'Left-arm fast-medium':('Left-arm','fast-medium'), 'Slow left-arm orthodox':('Left-arm','slow'), 'Right-arm fast':('Right-arm','fast'), 'Slow left-arm chinaman':('Left-arm','slow'), 'Left-arm medium-fast':('Left-arm','medium-fast'), 'Legbreak':(None,'legbreak'), 'Right-arm medium, Right-arm offbreak':('Right-arm','medium;offbreak'), 'Right-arm bowler':('Right-arm', None), 'Left-arm medium':('Left-arm','medium'), 'Right-arm offbreak, Legbreak googly':('Right-arm','(offbreak;legbreak)'), 'Right-arm medium, Legbreak':('Right-arm','medium;legbreak'), 'Right-arm offbreak, Legbreak':('Right-arm','offbreak;legbreak'), 'Left-arm fast':('Left-arm','fast') , '' : (None , None)}
+output = open('playersFinal.csv' , 'w')
+import csv
+with open('players.csv', 'rb') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	writer = csv.writer(output , delimiter = ',')
+	count = 0
+	#for row in reader:
+	#	if(row[9] not in l):
+	#		l.append(row[9])
+	#print l
+	
+	for row in reader:
+		bowl_attr = row.pop(9)
+		if(count == 0):
+			row.append('bowl_arm')
+			row.append('bowl_style')
+			count = count + 1
+		else:
+			row.append(mapping[bowl_attr][0])
+			row.append(mapping[bowl_attr][1])
+		writer.writerow(row)
+
+
+
+#print row[3]
+#print row[9]
