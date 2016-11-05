fd3323e9eabb612603a3115c5c06da76bc91146a
aditya9509 <Aditya.Naidu@iiitb.org>
2016-11-05 13:34:52 +0530

    Clustering trials/Minor modifications

--- final data generated/try.py
+++ final data generated/try.py
@@ -14,9 +14,9 @@ with open('batsman_vs_bowler_total.csv', 'rb') as csvfile:
 		if row[0] in excel:
 			excel[row[0]][0]+=int(row[10])
 			excel[row[0]][1]+=int(row[9])
-			excel[row[0]][2]+=int(row[14])
+			excel[row[0]][2]+=int(row[14])			
 		else:
-			excel[row[0]]= [int(row[10]),int(row[9]),int(row[14])]
+			excel[row[0]]= [int(row[10]),int(row[9]),int(row[14]) , row[11]]
 
 with open('average.csv','w+') as csvfile:
 	wr = csv.writer(csvfile)
@@ -27,4 +27,4 @@ with open('average.csv','w+') as csvfile:
 #			print excel
 			avg = excel[i][0]*1.0/excel[i][1]
 
-		wr.writerow([i,excel[i][0],excel[i][1],excel[i][2],(excel[i][0]*100.0)/excel[i][2],avg])
+		wr.writerow([i,excel[i][0],excel[i][1],excel[i][2],(excel[i][0]*100.0)/excel[i][2],avg,excel[i][3]])
--- scripts/Clustering R scripts/clusterTest.R
+++ scripts/Clustering R scripts/clusterTest.R
@@ -1,13 +1,20 @@
 library(fpc)
+
 data <- read.csv("/home/aditya9509/Cricket/final\ data\ generated/average.csv")
+
 data2 <- data.frame(data$Strike_rate,data$Avg)
+
+plot(data2)
+
+data[0,]
+
 select = 1
 
 if(select == 1){
   
   #-----------------K-means clustring -------------------------------#  
   
-  number_of_clusters = 2
+  number_of_clusters = 3
   
   number_of_samples = 60 # Set value to dim(data2)[1] to consider whole data.
   
@@ -17,11 +24,11 @@ if(select == 1){
   
   (kmeans.result <- kmeans(data2, number_of_clusters)) 
   
-  plot(data2[c("data.Strike_rate","data.Avg")] , col = kmeans.result$cluster)
+  plot(data2[c(colnames(data2)[1],colnames(data2)[2])] , col = kmeans.result$cluster)
   
   title(main = paste("K-means for " ,toString(number_of_samples) , " samples" ), sub = NULL, xlab = NULL, ylab = NULL,line = NA, outer = FALSE)
   
-  points(kmeans.result$centers[,c("data.Strike_rate","data.Avg")], col = 1:3, pch = 8, cex=2 )
+  points(kmeans.result$centers[,c(colnames(data2)[1],colnames(data2)[2])], col = 1:3, pch = 8, cex=2 )
   
   table(data[idx ,]$Name , kmeans.result$cluster)
 
@@ -31,28 +38,27 @@ if(select == 1){
   
   #---------------------Hierarchial clustering------------------------#
   
-  number_of_samples = 50
+  number_of_samples = 60
+  
+  number_of_tree_cuts  = 10
   
   idx <- sample(1:dim(data2)[1], number_of_samples)
   
   sampleData <- data2[idx ,]
   
-  hc <- hclust(dist(sampleData), method="ave" )
+  hc <- hclust(dist(sampleData), method="centroid" )
   
-  plot(hc, hang = -1 , labels= sampleData$data.Avg)
+  plot(hc, hang = -1 , labels= data[idx,]$batting_style)
   
   title(main = NULL, sub = NULL, xlab = NULL, ylab = NULL,line = NA, outer = FALSE)
   
+  rect.hclust(hc, k= number_of_tree_cuts)
   
-  k_1 = 3
-  
-  rect.hclust(hc, k=k_1)
-  
-  groups <- cutree(hc, k=k_1)
+  groups <- cutree(hc, k= number_of_tree_cuts)
   
   #-------------------------------------------------------------------#
   
-}else{
+}else if(select == 3){
 
 #--------------------Density based clustering------------------------#
 #-------------------------------------------------------------------#
